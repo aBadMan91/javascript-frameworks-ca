5e281c0c5d706a58e6c7ae692037f11b5f72c6ab
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2024-12-17 14:52:54 +0100

    Checkout Success added
    - Added Checkout Success page

--- src/pages/CheckoutSuccessPage/index.jsx
+++ src/pages/CheckoutSuccessPage/index.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 import * as styles from "./index.styles";
 
-export function CheckoutSuccessPage() {}
+export function CheckoutSuccessPage() {
+  return (
+    <styles.CheckoutSuccessContainer>
+      <h1>Checkout Successful!</h1>
+      <p>Thank you for your purchase. Your order has been placed successfully.</p>
+      <styles.StyledLink to="/">Go back to Home</styles.StyledLink>
+    </styles.CheckoutSuccessContainer>
+  );
+}
