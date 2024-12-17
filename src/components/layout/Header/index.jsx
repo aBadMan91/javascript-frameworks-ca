066bc95b643fed7312276cf0f98342023c845903
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2024-12-17 15:10:05 +0100

    Responsive header
    - Responsive header
    
    - Other smal changes

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { HeaderContainer, NavContainer } from "./index.styles";
+import { HeaderContainer, NavContainer, HamburgerMenu } from "./index.styles";
 import { SearchBar } from "../../SearchBar";
 import { CartIcon } from "../../CartIcon";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <HeaderContainer>
       <Link to="/">
         <h1>North Store</h1>
       </Link>
-      <SearchBar />
-      <NavContainer>
+      <HamburgerMenu onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </HamburgerMenu>
+      <NavContainer isOpen={isOpen}>
+        <SearchBar />
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/cart">
--- src/components/layout/Header/index.styles.js
+++ src/components/layout/Header/index.styles.js
@@ -12,6 +12,11 @@ export const HeaderContainer = styled.header`
     color: #fff;
     text-decoration: none;
   }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 export const NavContainer = styled.nav`
@@ -24,5 +29,31 @@ export const NavContainer = styled.nav`
     text-decoration: none;
     margin: 0 10px;
     cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  }
+`;
+
+export const HamburgerMenu = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  position: absolute;
+  right: 25px;
+  top: 35px;
+
+  span {
+    height: 2px;
+    width: 25px;
+    background: #fff;
+    margin: 3px 0;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
   }
 `;
